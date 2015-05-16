f624d27edef3f29ce33de178158f10ebf41b449f
msart <pedromarcondes94@hotmail.com>
2015-05-16 16:16:35 -0300

    Update main.c

--- final_files/main.c
+++ final_files/main.c
@@ -2,13 +2,12 @@
 
 int main(){
   int M, N, status;
-  char *nome_mapa, *log;
+  char *nome_mapa;
   char **mapa;
   barco b;
   
-	scanf("%s", nome_mapa);
-	log = "log.txt";
-	mapa = leia_mapa(nome_mapa, &M, &N);
+  scanf("%s", nome_mapa);
+  mapa = leia_mapa(nome_mapa, &M, &N);
   status = 1;	
   b = posiciona_barco(mapa, M,  N);
   escreva_mapa_tela(mapa, M, N);
@@ -17,29 +16,27 @@ int main(){
 	  status = rema_barco(mapa, M, N, b);
 	  if(status == 2){
 	      escreva_mapa_tela(mapa, M, N);
-        escreva_mapa_arquivo(log, mapa, M, N);
+              escreva_mapa_arquivo(log.txt, mapa, M, N);
 	      printf("VOCÊ VENCEU!\n");
 	      break;
 	   }
 	  if(status == 0){
 	      escreva_mapa_tela(mapa, M, N);
-        escreva_mapa_arquivo(log, mapa, M, N);
+              escreva_mapa_arquivo(log.txt, mapa, M, N);
 	      printf("GAME OVER!\n");
 	      break;
 	   }
-	  dispara_tiros(mapa, M, N, log, &status);
+	  dispara_tiros(mapa, M, N, log.txt, &status);
 	  if(status == 0){
 	      escreva_mapa_tela(mapa, M, N);
-        escreva_mapa_arquivo(log, mapa, M, N);	   
+              escreva_mapa_arquivo(log.txt, mapa, M, N);	   
 	      printf("GAME OVER!\n");
 	      break;
 	   }
 	  escreva_mapa_tela(mapa, M, N);
-    escreva_mapa_arquivo(log, mapa, M, N);
+          escreva_mapa_arquivo(log.txt, mapa, M, N);
     
   }
-	    
-	free(nome_mapa);
-	free(log);
-	destroi_mapa(mapa, M);
+  free(nome_mapa);
+  destroi_mapa(mapa, M);
 }
